2b33d2ad345f04436a30152fcc4b6bc16cba18a8
OttoAD <otavioad@hotmail.com>
2018-06-16 22:19:51 -0300

    Atualizando

--- app.js
+++ app.js
@@ -19,12 +19,26 @@ app.use(express.static('public'));
 //set the views folder for pages (.html and .ejs files)
 app.set('views', path.join(__dirname , '/views'));
 
+// ----- ROUTES -----
 //route to index page
 app.get('/', function (request, response) {
-    console.log("Got a GET request for the homepage");
+    console.log("GET request for the homepage");
     response.render('index.ejs');
  });
 
+ //route to login page
+ app.get('/login', function (request, response){
+    console.log("GET request for the login page");
+    response.render('login.ejs');
+});
+
+ //route to sign up page
+ app.get('/signup', function (request, response){
+    console.log("GET request for the sign up page");
+    response.render('signup.ejs');
+ });
+
+ // ----- SERVER -----
 //server variable that listens in the specified port
 var server = app.listen(port, function () {
 
