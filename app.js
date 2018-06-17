6d9d28a075de52640dce9a667638e908d5f50956
OttoAD <otavioad@hotmail.com>
2018-06-16 22:49:12 -0300

    Atualizando

--- app.js
+++ app.js
@@ -4,45 +4,28 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+//set the routes
+var routes = require("./routes/index");
+
 //set the server port
 var port = 8081;
 
 //our app variable using express module
 var app = express();
 
+// ----- View engine setup -----
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
 //use the public folder as static folder
 app.use(express.static('public'));
-
 //set the views folder for pages (.html and .ejs files)
 app.set('views', path.join(__dirname , '/views'));
 
-// ----- ROUTES -----
-//route to index page
-app.get('/', function (request, response) {
-    console.log("GET request for the homepage");
-    response.render('index.ejs');
- });
+//Setting the used routes
+app.use('/', routes);
 
- //route to login page
- app.get('/login', function (request, response){
-    console.log("GET request for the login page");
-    response.render('login.ejs');
-});
-
- //route to sign up page
- app.get('/signup', function (request, response){
-    console.log("GET request for the sign up page");
-    response.render('signup.ejs');
- });
-
- // ----- SERVER -----
+// ----- SERVER -----
 //server variable that listens in the specified port
-var server = app.listen(port, function () {
-
-    var host = server.address().address;
- 
-    console.log("Listening at http://%s:%s", host, port);
- })
+var server = app.listen(port, function () { 
+    console.log("Listening at http://localhost:%s", port);
+ });
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,27 @@ var router = express.Router();
 
 // Home page
 router.get('/',function(request, response, next){
-    
+    console.log("GET request for the home page");
+
+    response.render('index.ejs',{
+       title: 'Minha Agenda'
+   }); 
 });
+
+ //route to login page
+router.get('/login', function (request, response, next){
+    console.log("GET request for the login page");
+    response.render('login.ejs',{
+        title: 'Login'
+    });
+});
+
+ //route to sign up page
+ router.get('/signup', function (request, response, next){
+    console.log("GET request for the sign up page");
+    response.render('signup.ejs', {
+        title: 'Sign Up'
+    });
+ });
+
+module.exports = router;
