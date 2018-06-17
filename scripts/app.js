f6b74dcf6284330162f4bb31cffbeac21cb17d8f
tuliolima <tulioabner@gmail.com>
2018-06-17 15:24:56 -0300

    Dialog para adição de eventos

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,17 @@
         events: {},
         news: {},
         eventTemplate: document.querySelector('.event-template'),
-        eventsList: document.querySelector('.events-list')
+        eventsList: document.querySelector('.events-list'),
+        addEventDialog: document.getElementById('addEventDialog')
     };
 
-    document.getElementById('button-add-event').addEventListener('click', function () {
-        // Abre uma dialog de criação de eventos.
-        // Por enquanto a criação está direta.
-        // TODO app.toggleAddDialog(true);
+    document.getElementById('button-cancel-event').addEventListener('click', function () {
+        addEventDialog.close();
+        // TODO limpar dados
+    })
+
+    document.getElementById('button-confirm-event').addEventListener('click', function() {
+        // TODO pegar dados
         var event;
         var title = "Prova de Cálculo 1"
         event = app.eventTemplate.cloneNode(true);
@@ -18,5 +22,13 @@
         event.removeAttribute('hidden');
         app.eventsList.appendChild(event);
         app.events[1] = event;
+        addEventDialog.close();
+    })
+
+    document.getElementById('button-add-event').addEventListener('click', function () {
+        // Abre uma dialog de criação de eventos.
+        // Por enquanto a criação está direta.
+        // TODO app.toggleAddDialog(true);
+        addEventDialog.showModal();
     });
 })();
