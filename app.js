5346b1278af4f1ae7b4ca5b53c8420aeeef2dedf
OttoAD <otavioad@hotmail.com>
2018-06-16 21:17:17 -0300

    Atualizando

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
+//import modules
 var express = require('express');
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var path = require("path");
 
+//set the server port
+var port = 8081;
+
+//our app variable using express module
 var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//use the public folder as static folder
 app.use(express.static('public'));
+
+//set the views folder for pages (.html and .ejs files)
 app.set('views', path.join(__dirname , '/views'));
 
+//route to index page
 app.get('/', function (request, response) {
     console.log("Got a GET request for the homepage");
     response.render('index.ejs');
  });
 
-
-var server = app.listen(8081, function () {
+//server variable that listens in the specified port
+var server = app.listen(port, function () {
 
     var host = server.address().address;
-    var port = server.address().port;
  
     console.log("Listening at http://%s:%s", host, port);
  })
