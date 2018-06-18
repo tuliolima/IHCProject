47364a885da2a40c9295b307e037340845e0163a
tuliolima <tulioabner@gmail.com>
2018-06-17 21:13:26 -0300

    Script do signup

--- public/javascripts/dashboardPage.js
+++ public/javascripts/dashboardPage.js
@@ -12,16 +12,10 @@
         // TODO limpar dados
     })
 
-    document.getElementById('button-confirm-event').addEventListener('click', function() {
+    document.getElementById('button-confirm-event').addEventListener('click', function () {
         // TODO pegar dados
-        var event;
         var title = "Prova de Cálculo 1"
-        event = app.eventTemplate.cloneNode(true);
-        event.classList.remove('cardTemplate');
-        event.querySelector('.title').textContent = title;
-        event.removeAttribute('hidden');
-        app.eventsList.appendChild(event);
-        app.events[1] = event;
+        createEvent(title, '', '', '', 1);
         addEventDialog.close();
     })
 
@@ -31,4 +25,46 @@
         // TODO app.toggleAddDialog(true);
         addEventDialog.showModal();
     });
+
+    var fakeEvents = [
+        {
+            title: 'Prova de IHC',
+            description: 'fdsfasfd',
+            place: 'UnB',
+            date: '28/03/2014',
+            type: 1
+        },
+        {
+            title: 'Apresentação de trabalho',
+            description: '',
+            place: 'Universidade de Brasília',
+            date: '28/03/2014',
+            type: 2
+        }
+    ]
+
+    function updateEvents() {
+        // Baixar os eventos do usuário do servidor
+        // e atualizar na página.
+        // TODO acessar o servidor
+        fakeEvents.forEach(element => {
+            createEvent(element.title, element.description, element.place, element.date, element.type);
+        });
+    }
+
+    function createEvent(title, description, place, date, type) {
+        var event = app.eventTemplate.cloneNode(true);
+        event.querySelector('.title').textContent = title;
+        event.querySelector('.description').textContent = description;
+        event.querySelector('.place').textContent = place;
+        event.querySelector('.date').textContent = date;
+        // TODO Change event color
+        event.classList.remove('cardTemplate');
+        event.removeAttribute('hidden');
+        app.eventsList.appendChild(event);
+        app.events[app.events.length] = event;
+    }
+
+    // Code starts here:
+    updateEvents();
 })();
---
+++ public/javascripts/signupPage.js
@@ -0,0 +1,14 @@
+// Comportamento da página de signup
+
+(function() {
+    document.getElementById('button-finish_signup').addEventListener('click', function () {
+        var name = document.getElementById('name').textContent;
+        var user = document.getElementById('user').textContent;
+        var password = document.getElementById('password').textContent;
+        var rePassword = document.getElementById('re-password').textContent;
+        var email = document.getElementById('email').textContent;
+        
+        // TODO dar post no servidor
+        // TODO limpar dados
+    })
+})
