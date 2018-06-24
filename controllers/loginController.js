825a51cce4493fa2c660f8399a6d637fbcdf2992
OttoAD <otavioad@hotmail.com>
2018-06-24 01:54:24 -0300

    Alterações

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,42 +3,49 @@ var User = require('../models/user');
 // Display user create form on GET.
 exports.login_get = function (request, response) {
     console.log("GET request for the login page");
-    response.render('login.ejs', {
-        title: 'Login'
-    });
+    if(request.session.user){
+        response.redirect('/');
+    }else{
+        response.render('login.ejs', {
+            title: 'Login'
+        });
+    }
 };
 
 // Handle login on POST.
 exports.login_post = function (request, response) {
     console.log("POST request for the login page");
+    if(request.session.user){
+        response.redirect('/');
+    }else{
+        var username = request.body.username;
+        var password = request.body.password;
 
-    var username = request.body.username;
-    var password = request.body.password;
+        User.findOne({username: username},function(err,user){
+            if(err){
+                console.log("Erro ao encontrar usuario no banco");
+                return response.status(500).send();
+            }
 
-    User.findOne({username: username},function(err,user){
-        if(err){
-            console.log("Erro ao encontrar usuario no banco");
-            return response.status(500).send();
-        }
-
-        if(!user){
-            console.log("Usuario inexistente no banco");
-            return response.status(401).send();
-        } else if (!user.validPassword(request.body.password)) {
-            console.log("Senha invalida");
-            return response.status(401).send();
-        } else {
-            request.session.user = user;
-            console.log("Logged in");
-            response.redirect('/');
-            return response.status(200).send();
-        }
-    });
+            if(!user){
+                console.log("Usuario inexistente no banco");
+                return response.status(401).send();
+            } else if (!user.validPassword(request.body.password)) {
+                console.log("Senha invalida");
+                return response.status(401).send();
+            } else {
+                request.session.user = user;
+                console.log("Logged in");
+                return response.status(200).send();
+            }
+        });
+    }
 
 };
 
 //Handle user logout on GET
 exports.user_logout_get = function(request,response){
+    response.clearCookie('user_id');
     request.session.destroy();
     console.log("Logged out");
     response.redirect('/login');
--- controllers/signupController.js
+++ controllers/signupController.js
@@ -11,30 +11,22 @@ exports.user_create_get = function (req, res) {
 // Handle user create on POST.
 exports.user_create_post = function (req, res) {
     console.log("POST request for the sign up page");
-   // var item = {
-       // name: req.body.name,
-        //date_of_birth: req.body.date_of_birth,
-        //gender: req.body.gender,
-        //handicap: req.body.handicap,
-        //profession: req.body.profession,
-      //  email: req.body.email,
-      //  password: req.body.password,
-      //  username: req.body.username
-        //interests: req.body.interests,
-        //events: req.body.events
-    //};
-    var data = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        username: req.body.username
-    });
-    
-    data.save(function (error) {
-        if (error) {
-            console.log('Usuário não cadastrado: %s', error.message);
-        } else {
-            console.log('Usuário cadastrado com sucesso');
-        }
-    });
+    if(request.session.user){
+        response.redirect('/');
+    }else{
+        var data = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            username: req.body.username
+        });
+        
+        data.save(function (error) {
+            if (error) {
+                console.log('Usuário não cadastrado: %s', error.message);
+            } else {
+                console.log('Usuário cadastrado com sucesso');
+            }
+        });
+    }
 };
